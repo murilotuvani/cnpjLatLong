abc8fee279e6580779da72a3ee44af3c2b681c32
Murilo Tuvani <murilo@autogeral.com.br>
2020-06-24 00:37:04 -0300

    [update] apenas formatacao padrao do NetBeans

--- src/test/java/net/cartola/cnpj/tolatlong/CnpjDaoTest.java
+++ src/test/java/net/cartola/cnpj/tolatlong/CnpjDaoTest.java
@@ -2,8 +2,6 @@ package net.cartola.cnpj.tolatlong;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import org.junit.After;
@@ -20,10 +18,10 @@ import static org.junit.Assert.*;
 public class CnpjDaoTest {
 
     private static ConnectionFactory connFactory;
-    
+
     public CnpjDaoTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
         CnpjLatLongConfig config = new CnpjLatLongConfig();
@@ -33,16 +31,16 @@ public class CnpjDaoTest {
         config.setBdSenha("root");
         connFactory = new ConnectionFactory(config);
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
-        
+
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -55,39 +53,33 @@ public class CnpjDaoTest {
         Connection conn = connFactory.getConnection();
         CnpjDao instance = new CnpjDao(conn);
         long cnpj = 5437537000137L;
-        List<Cnpj> cnpjs = instance.listar("\n WHERE cnpj="+cnpj);
+        List<Cnpj> cnpjs = instance.listar("\n WHERE cnpj=" + cnpj);
         assertEquals(1, cnpjs.size());
         assertEquals(cnpj, cnpjs.get(0).getCnpj());
     }
-    
+
     // LATITUDE: -23.25638008
     // LONGITUDE: -47.30302811
-    
     @Test
     public void testUpdate() throws SQLException {
         Connection conn = connFactory.getConnection();
         CnpjDao instance = new CnpjDao(conn);
         long cnpj = 5437537000137L;
-        List<Cnpj> cnpjs = instance.listar("\n WHERE cnpj="+cnpj);
+        List<Cnpj> cnpjs = instance.listar("\n WHERE cnpj=" + cnpj);
         Cnpj ocnpj = cnpjs.get(0);
         assertEquals(1, cnpjs.size());
         assertEquals(cnpj, ocnpj.getCnpj());
-        
+
         BigDecimal lat = new BigDecimal("-23.25638008");
         BigDecimal lng = new BigDecimal("-47.30302811");
         ocnpj.setLatitude(lat);
         ocnpj.setLongitude(lng);
         assertEquals(1, instance.update(ocnpj));
-        
-        cnpjs = instance.listar("\n WHERE cnpj="+cnpj);
+
+        cnpjs = instance.listar("\n WHERE cnpj=" + cnpj);
         ocnpj = cnpjs.get(0);
         assertEquals(lat, ocnpj.getLatitude());
         assertEquals(lng, ocnpj.getLongitude());
-        
-        
-        
-        
     }
 
-    
 }
